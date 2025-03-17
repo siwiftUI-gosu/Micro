e905ce162416ff10cec4a6fe056e3ca090772c19
JunYoung <ghddns34@gmail.com>
2025-03-17 22:01:59 +0900

    IOS-38

---
+++ Micro/AddBookCompleteView/AddBookCompleteView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBookCompleteView.swift
+//  Micro
+//
+//  Created by SeoJunYoung on 3/17/25.
+//
+
+import SwiftUI
+
+struct AddBookCompleteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookCompleteView()
+}
